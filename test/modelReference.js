feb51e9494e96ce1db54d7b30487eb477ff1b8b9
Peter Fesz-Nguyen <pfesznguyen@gmail.com>
2018-02-12 22:18:24 -0700

    supports document referencing and embedding

---
+++ test/modelReference.js
@@ -0,0 +1,198 @@
+const path = require('path')
+const fs = require('fs')
+const promisify = require('util').promisify
+const unlink = promisify(fs.unlink)
+
+const test = require('tape')
+
+fs.readdir(path.join(__dirname, '../db'), (err, files) => {
+	const deleteFiles = files
+							.filter(file => file.startsWith('ko_db_test'))
+							.map(file => unlink(path.join(__dirname, '../db', file)))
+
+	Promise.all(deleteFiles).then(runTests)
+})
+
+test.onFinish(() => {
+	fs.readdir(path.join(__dirname, '../db'), (err, files) => {
+		files
+			.filter(file => file.startsWith('ko_db_test'))
+			.forEach(file => unlink(path.join(__dirname, '../db', file)))
+	})
+})
+
+
+function runTests () {
+	const ko = require('../ko')
+
+	test('Model creation succeeds when schema contains references (created one at a time)', function (t) {
+		try {
+			ko.models({
+				ko_db_test_ref1: {
+					field: ko.String
+				}
+			})
+			ko.models({
+				ko_db_test_test1: {
+					ref: ko.models.ko_db_test_ref1.ref
+				}
+			})
+			t.pass('Created models successfully')
+			t.end()
+		} catch (err) {
+			t.error(err)
+			t.end()
+		}
+	})
+
+	test('Model creation succeeds when schema contains references (created together)', function (t) {
+		try {
+			ko.models({
+				ko_db_test_ref2: {
+					field: ko.String
+				},
+				ko_db_test_test2: {
+					ref: ko.models.ko_db_test_ref2.ref
+				}
+			})
+			t.pass('Created models successfully')
+			t.end()
+		} catch (err) {
+			t.error(err)
+			t.end()
+		}
+	})
+
+	test('Validation works for schemas that contain references', function (t) {
+		try {
+			ko.models({
+				ko_db_test_ref3: {
+					field: ko.String
+				},
+				ko_db_test_test3: {
+					ref: ko.models.ko_db_test_ref3.ref
+				}
+			})
+			ko.models.ko_db_test_ref3.create({
+				_id: '0',
+				field: 'value'
+			}).save().then(ref => {
+				return ko.models.ko_db_test_test3.validate({
+					ref: '0'
+				})
+			}).then(() => {
+				// resolving promise means validation succeeded
+				t.pass('Valid reference passes validation')
+			}).then(() => {
+				ko.models.ko_db_test_test3.validate({
+					ref: '1'
+				}).then(() => {
+					t.fail('Invalid reference passed validation')
+					t.end()
+				}).catch(err => {
+					// rejected promise means validation failed
+					t.equal(typeof err, 'object', 'Invalid reference fails validation')
+					t.equal('ref' in err, true, 'Error contains correct field')
+					t.end()
+				})
+			}).catch(err => {
+				console.error('unexpected error')
+				t.error(err)
+				t.end()
+			})
+		} catch (err) {
+			t.error(err)
+			t.end()
+		}
+	})
+
+	test('Validation works for schemas that contain embedded references', function (t) {
+		ko.models({
+			ko_db_test_ref4: {
+				field: ko.String
+			},
+			ko_db_test_test4: {
+				ref: ko.models.ko_db_test_ref4.embed
+			}
+		})
+		ko.models.ko_db_test_test4.validate({
+			ref: {
+				field: 'valid!'
+			}
+		}).then(() => {
+			// resolved promise means validation passed
+			t.pass('Valid embed passed validation')
+			ko.models.ko_db_test_test4.validate({
+				ref: {
+					field: 100
+				}
+			}).then(() => {
+				// resolved promise means validation passed
+				t.fail('Invalid embed passed validation')
+				t.end()
+			}).catch(err => {
+				// rejected promise means validation failed
+				t.equal(typeof err, 'object', 'Invalid embed fails validation')
+				t.equal('ref' in err, true, 'Error contains correct field')
+				t.end()
+			})
+		}).catch(err => {
+			console.error('unexpected error')
+			t.error(err)
+			t.end()
+		})
+	})
+
+	test('Saving works for schemas that contain references', function (t) {
+		ko.models({
+			ko_db_test_ref5: {
+				field: ko.String
+			},
+			ko_db_test_test5: {
+				ref: ko.models.ko_db_test_ref5.ref
+			}
+		})
+		ko.models.ko_db_test_ref5.create({
+			field: 'value'
+		}).save().then(ref => {
+			return ko.models.ko_db_test_test5.create({
+				ref: ref._id
+			}).save()
+		})
+		.then(doc => {
+			t.pass('Successfully created document')
+			t.end()
+		}).catch(err => {
+			t.error(err, 'Failed to save the document')
+			t.end()
+		})
+	})
+
+	test('Saving works for schemas that contain embedded references', function (t) {
+		ko.models({
+			ko_db_test_ref6: {
+				field: ko.String
+			},
+			ko_db_test_test6: {
+				ref: ko.models.ko_db_test_ref6.embed
+			}
+		})
+		ko.models.ko_db_test_test6.create({
+			ref: {
+				field: 'In the database'
+			}
+		}).save().then(doc => {
+			t.pass('Successfully created document')
+			t.equal('_id' in doc.ref, true, 'Embedded document received an id')
+			return ko.models.ko_db_test_ref6.count({_id: doc.ref._id})
+		})
+		.then(count => {
+			t.equal(count, 1, 'Embedded document was saved to the database')
+			t.end()
+		}).catch(err => {
+			t.error(err, 'Failed to save the document')
+			t.end()
+		})
+	})
+}
+
--- test/typeclass.js
+++ test/typeclass.js
@@ -13,37 +13,130 @@ test('Typeclass.getBasicType should work for basic types', function (t) {
 
 
 test('String typetypes should validate correctly', function (t) {
-	t.equal(Typeclass.types.String.check('fdsa'), true, 'Base String type')
-	t.equal(Typeclass.types.String.check(123), false, 'Base type returns false when not a String')
-	t.equal(Typeclass.types.String[4].check('fdsa'), true, 'Length limited String type')
-	t.equal(Typeclass.types.String[4].check('12345'), false, 'Length limited String type returns false when too long')
-	t.equal(Typeclass.types.String.constant('fdsa').check('fdsa'), true, 'Constant String which matches')
-	t.equal(Typeclass.types.String.constant('fdsa').check(undefined), true, 'Constant String passed undefined')
-	t.equal(Typeclass.types.String.constant('fdsa').check(null), true, 'Constant String passed null')
-	t.equal(Typeclass.types.String.constant('fdsa').check('aaaa'), false, 'Constant String which does not match')
-	t.equal(Typeclass.types.String.optional().check('fdsa'), true, 'Optional passed a string')
-	t.equal(Typeclass.types.String.optional().check(null), true, 'Optional passed null')
-	t.equal(Typeclass.types.String.optional().check(123), false, 'Optional passed non matching value')
-	t.end()
+	t.plan(13)
+	Typeclass.types.String.check('fdsa').then(valid => {
+		t.equal(valid, true, 'Base String type')
+	})
+	Typeclass.types.String.check(123).then(valid => {
+		t.equal(valid, false, 'Base type returns false when not a String')
+	})
+	Typeclass.types.String[4].check('fdsa').then(valid => {
+		t.equal(valid, true, 'Length limited String type')
+	})
+	Typeclass.types.String[4].check('12345').then(valid => {
+		t.equal(valid, false, 'Length limited String type returns false when too long')
+	})
+	Typeclass.types.String[4].check('12345').then(valid => {
+		t.equal(valid, false, 'Length limited String type returns false when too long')
+	})
+	Typeclass.types.String.constant('fdsa').check('fdsa').then(valid => {
+		t.equal(valid, true, 'Constant String which matches')
+	})
+	Typeclass.types.String.constant('fdsa').check(undefined).then(valid => {
+		t.equal(valid, true, 'Constant String passed undefined')
+	})
+	Typeclass.types.String.constant('fdsa').check(null).then(valid => {
+		t.equal(valid, true, 'Constant String passed null')
+	})
+	Typeclass.types.String.constant('fdsa').check(null).then(valid => {
+		t.equal(valid, true, 'Constant String passed null')
+	})
+	Typeclass.types.String.constant('fdsa').check('aaaa').then(valid => {
+		t.equal(valid, false, 'Constant String which does not match')
+	})
+	Typeclass.types.String.optional().check('fdsa').then(valid => {
+		t.equal(valid, true, 'Optional passed a string')
+	})
+	Typeclass.types.String.optional().check(null).then(valid => {
+		t.equal(valid, true, 'Optional passed null')
+	})
+	Typeclass.types.String.optional().check(123).then(valid => {
+		t.equal(valid, false, 'Optional passed non matching value')
+	})
 })
 
 
 test('Number typetypes should validate correctly', function (t) {
-	t.equal(Typeclass.types.Number.check(123), true, 'Base Number type')
-	t.equal(Typeclass.types.Number.check('fdsa'), false, 'Base Number type returns false when not a String')
-	t.equal(Typeclass.types.Number.min(0).check(123), true, 'Min value that passes')
-	t.equal(Typeclass.types.Number.min(0).check(-123), false, 'Min value that does not pass')
-	t.equal(Typeclass.types.Number.min(0).max(123).check(123), true, 'Min and Max value that passes')
-	t.end()
+	t.plan(5)
+	Typeclass.types.Number.check(123).then(valid => {
+		t.equal(valid, true, 'Base Number type')
+	})
+	Typeclass.types.Number.check('fdsa').then(valid => {
+		t.equal(valid, false, 'Base Number type returns false when not a Number')
+	})
+	Typeclass.types.Number.min(0).check(123).then(valid => {
+		t.equal(valid, true, 'Min value that passes')
+	})
+	Typeclass.types.Number.min(0).check(-123).then(valid => {
+		t.equal(valid, false, 'Min value that does not pass')
+	})
+	Typeclass.types.Number.min(0).max(123).check(123).then(valid => {
+		t.equal(valid, true, 'Min and Max value that passes')
+	})
+})
+
+test('Array types should validate correctly', function (t) {
+	t.plan(7)
+	const arrayType = Typeclass.types.Array(Typeclass.types.Number)
+	arrayType.check([]).then(valid => {
+		t.equal(valid, true, 'Array type works for empty array')
+	})
+	arrayType.check([0]).then(valid => {
+		t.equal(valid, true, 'Array type works for array of one element')
+	})
+	arrayType.check([1, 2, 3]).then(valid => {
+		t.equal(valid, true, 'Array type works for array of multiple elements')
+	})
+	arrayType.check(['hello']).then(valid => {
+		t.equal(valid, false, 'Array type returns false for non matching value')
+	})
+	arrayType.check([0, 'hello']).then(valid => {
+		t.equal(valid, false, 'Array type returns false for mixed values')
+	})
+	arrayType.notEmpty().check([0]).then(valid => {
+		t.equal(valid, true, 'notEmpty validator passes')
+	})
+	arrayType.notEmpty().check([]).then(valid => {
+		t.equal(valid, false, 'notEmpty validator fails for empty array')
+	})
+})
+
+test('Array of options should validate correctly', function (t) {
+	t.plan(5)
+	const arrayType = Typeclass.types.Array(Typeclass.types.Option([
+		Typeclass.types.Number,
+		Typeclass.types.String
+	]))
+	arrayType.check([]).then(valid => {
+		t.equal(valid, true, 'Option-Array type works for empty array')
+	})
+	arrayType.check(['Hello']).then(valid => {
+		t.equal(valid, true, 'Option-Array type works for single matching element')
+	})
+	arrayType.check([0]).then(valid => {
+		t.equal(valid, true, 'Option-Array type works for single matching element')
+	})
+	arrayType.check([0, 'Hello']).then(valid => {
+		t.equal(valid, true, 'Option-Array type works for mixed types')
+	})
+	arrayType.check([0, 'Hello', null]).then(valid => {
+		t.equal(valid, false, 'Option-Array type returns false for nonmatching values')
+	})
 })
 
 test('Embedded documents should validate correctly', function (t) {
+	t.plan(3)
 	const documentType = Typeclass.types.Document({
 		name: Typeclass.types.String,
 		age: Typeclass.types.Number
 	})
-	t.equal(documentType.check({name: 'John', age: 250}), true, 'Matching subdocument type')
-	t.equal(documentType.check({name: 250, age: 'John'}), false, 'Incompatible subdocument types')
-	t.equal(documentType.check({foo: 'bar'}), false, 'Incompatible subdocument properties')
-	t.end()
+	documentType.check({name: 'John', age: 250}).then(valid => {
+		t.equal(valid, true, 'Matching subdocument type')
+	})
+	documentType.check({name: 250, age: 'John'}).then(valid => {
+		t.equal(valid, false, 'Incompatible subdocument types')
+	})
+	documentType.check({foo: 'bar'}).then(valid => {
+		t.equal(valid, false, 'Incompatible subdocument properties')
+	})
 })
